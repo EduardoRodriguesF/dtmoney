0502218ab9cb6b356af57fd55728564cd58655c5
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-03-18 08:19:03 -0300

    refactor: modal to separated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal'
 import { GlobalStyle } from './styles/global';
 
-Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -22,12 +21,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal 
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </div>
